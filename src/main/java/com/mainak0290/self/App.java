6a501ab80f0c987e9c16c19d31174dbc5afae512
Mainak0290 <mainkskdr@gmail.com>
2022-04-09 12:31:46 +0530

    Sample test project

---
+++ src/main/java/com/mainak0290/self/App.java
@@ -0,0 +1,11 @@
+package com.mainak0290.self;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int sum(int a, int b) { return a+b; }
+
+}
---
+++ src/test/java/com/mainak0290/self/AppTest.java
@@ -0,0 +1,20 @@
+package com.mainak0290.self;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( App.sum(2,1)==2 );
+    }
+}
